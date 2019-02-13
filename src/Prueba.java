915587887c713418f6e7a7987733fa0c1951d242
Guadalupe Vazquez <guvadlt@gmail.com>
2019-02-13 16:47:19 -0600

    Se agrego la clase ConversorTemperaturas

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,4 +1,15 @@
 
+class ConversorTemperaturas{
+	
+	public double centigradosAFahrenheit(double gradosC){
+		return gradosC*1.8+32;
+	}
+	public double fahrenheitACentigrados(double gradosF){
+		return (gradosF-32)/1.8;
+	}
+}
+
+
 public class Prueba {
 
 	public static void main(String[] args) {
